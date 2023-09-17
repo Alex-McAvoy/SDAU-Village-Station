ec91b14d35f7544c01ab905640f68cf0751a68f9
Alex-McAvoy <931772462@qq.com>
2023-09-17 08:24:13 +0800

    Add files via upload

---
+++ VillageStationApp/config.js
@@ -0,0 +1,12 @@
+// 应用全局配置
+module.exports = {
+  baseUrl: 'https://vue.ruoyi.vip/prod-api',
+  // baseUrl: 'http://localhost:8080',
+  // 应用信息
+  appInfo: {
+    // 应用名称
+    name: "village-station-app",
+    // 应用版本
+    version: "1.0.0",
+  }
+}
