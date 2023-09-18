04cdbf938c5ea1390f9ee2b6f037816e74de8ce8
Ruzhu <Ruhzu>
2023-09-18 10:36:03 +0800

    提交测试

--- VillageStationApp/config.js
+++ VillageStationApp/config.js
@@ -2,9 +2,10 @@
 module.exports = {
   baseUrl: 'https://vue.ruoyi.vip/prod-api',
   // baseUrl: 'http://localhost:8080',
-  // 应用信息
+  // 应用信息-----test
+  改变测试
   appInfo: {
-    // 应用名称 test
+    // 应用名称 提交测试2
     name: "village-station-app",
     // 应用版本
     version: "1.0.0",
