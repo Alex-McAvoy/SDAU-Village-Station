fcc10c996a87b9928945f25ca0c3bdc210f1e2dc
L666888999 <1410009393.@qq.com>
2023-10-09 21:51:52 +0800

    Add files via upload

---
+++ VillageStationBackend/station/src/main/java/com/ruoyi/system/controller/TbFinanceController.java
@@ -0,0 +1,115 @@
+package com.ruoyi.system.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.system.domain.TbFinance;
+import com.ruoyi.system.service.ITbFinanceService;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.page.TableDataInfo;
+
+/**
+ * 金融Controller
+ * 
+ * @author ruoyi
+ * @date 2023-10-09
+ */
+@RestController
+@RequestMapping("/system/finance")
+public class TbFinanceController extends BaseController
+{
+    @Autowired
+    private ITbFinanceService tbFinanceService;
+
+    /**
+     * 查询金融列表
+     */
+    @PreAuthorize("@ss.hasPermi('system:finance:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(TbFinance tbFinance)
+    {
+        startPage();
+        List<TbFinance> list = tbFinanceService.selectTbFinanceList(tbFinance);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出金融列表
+     */
+    @PreAuthorize("@ss.hasPermi('system:finance:export')")
+    @Log(title = "金融", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, TbFinance tbFinance)
+    {
+        List<TbFinance> list = tbFinanceService.selectTbFinanceList(tbFinance);
+        ExcelUtil<TbFinance> util = new ExcelUtil<TbFinance>(TbFinance.class);
+        util.exportExcel(response, list, "金融数据");
+    }
+
+    /**
+     * 获取金融详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('system:finance:query')")
+    @GetMapping(value = "/id/{newsId}")
+    public AjaxResult getInfo(@PathVariable("newsId") Long newsId)
+    {
+        return success(tbFinanceService.selectTbFinanceByNewsId(newsId));
+    }
+
+    /**
+     * 根据firstColumn获取金融详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('system:finance:query')")
+    @GetMapping(value = "/column/{firstColumn}")
+    public AjaxResult getFirstColumnInfo(@PathVariable("firstColumn") String firstColumn)
+    {
+        return success(tbFinanceService.selectTbFinanceByFirstColumn(firstColumn));
+    }
+
+
+    /**
+     * 新增金融
+     */
+    @PreAuthorize("@ss.hasPermi('system:finance:add')")
+    @Log(title = "金融", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody TbFinance tbFinance)
+    {
+        return toAjax(tbFinanceService.insertTbFinance(tbFinance));
+    }
+
+    /**
+     * 修改金融
+     */
+    @PreAuthorize("@ss.hasPermi('system:finance:edit')")
+    @Log(title = "金融", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody TbFinance tbFinance)
+    {
+        return toAjax(tbFinanceService.updateTbFinance(tbFinance));
+    }
+
+    /**
+     * 删除金融
+     */
+    @PreAuthorize("@ss.hasPermi('system:finance:remove')")
+    @Log(title = "金融", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{newsIds}")
+    public AjaxResult remove(@PathVariable Long[] newsIds)
+    {
+        return toAjax(tbFinanceService.deleteTbFinanceByNewsIds(newsIds));
+    }
+}
---
+++ VillageStationBackend/station/src/main/java/com/ruoyi/system/service/impl/TbFinanceServiceImpl.java
@@ -0,0 +1,110 @@
+package com.ruoyi.system.service.impl;
+
+import java.util.List;
+import com.ruoyi.common.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ruoyi.system.mapper.TbFinanceMapper;
+import com.ruoyi.system.domain.TbFinance;
+import com.ruoyi.system.service.ITbFinanceService;
+
+/**
+ * 金融Service业务层处理
+ * 
+ * @author ruoyi
+ * @date 2023-10-09
+ */
+@Service
+public class TbFinanceServiceImpl implements ITbFinanceService 
+{
+    @Autowired
+    private TbFinanceMapper tbFinanceMapper;
+
+    /**
+     * 查询金融
+     * 
+     * @param newsId 金融主键
+     * @return 金融
+     */
+    @Override
+    public TbFinance selectTbFinanceByNewsId(Long newsId)
+    {
+        return tbFinanceMapper.selectTbFinanceByNewsId(newsId);
+    }
+
+    /**
+     * 根据firstColumn查询金融
+     *
+     * @param firstColumn
+     * @return 金融
+     */
+    @Override
+    public List<TbFinance> selectTbFinanceByFirstColumn(String firstColumn)
+    {
+        return tbFinanceMapper.selectTbFinanceByFirstColumn(firstColumn);
+    }
+
+
+
+    /**
+     * 查询金融列表
+     * 
+     * @param tbFinance 金融
+     * @return 金融
+     */
+    @Override
+    public List<TbFinance> selectTbFinanceList(TbFinance tbFinance)
+    {
+        return tbFinanceMapper.selectTbFinanceList(tbFinance);
+    }
+
+    /**
+     * 新增金融
+     * 
+     * @param tbFinance 金融
+     * @return 结果
+     */
+    @Override
+    public int insertTbFinance(TbFinance tbFinance)
+    {
+        tbFinance.setCreateTime(DateUtils.getNowDate());
+        return tbFinanceMapper.insertTbFinance(tbFinance);
+    }
+
+    /**
+     * 修改金融
+     * 
+     * @param tbFinance 金融
+     * @return 结果
+     */
+    @Override
+    public int updateTbFinance(TbFinance tbFinance)
+    {
+        tbFinance.setUpdateTime(DateUtils.getNowDate());
+        return tbFinanceMapper.updateTbFinance(tbFinance);
+    }
+
+    /**
+     * 批量删除金融
+     * 
+     * @param newsIds 需要删除的金融主键
+     * @return 结果
+     */
+    @Override
+    public int deleteTbFinanceByNewsIds(Long[] newsIds)
+    {
+        return tbFinanceMapper.deleteTbFinanceByNewsIds(newsIds);
+    }
+
+    /**
+     * 删除金融信息
+     * 
+     * @param newsId 金融主键
+     * @return 结果
+     */
+    @Override
+    public int deleteTbFinanceByNewsId(Long newsId)
+    {
+        return tbFinanceMapper.deleteTbFinanceByNewsId(newsId);
+    }
+}
