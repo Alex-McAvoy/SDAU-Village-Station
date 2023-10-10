bb6bc87ffadece809239e3f20b9d17379b1438e7
Qgs123 <2313039289@qq.com>
2023-10-10 22:54:40 +0800

    Add files

--- VillageStationApp/pages/station/onlinebase.vue
+++ VillageStationApp/pages/station/onlinebase.vue
@@ -4,101 +4,129 @@
 			<view @click="echarts.onClick" :prop="option" :change:prop="echarts.updateEcharts" id="echartsmap"
 				class="echarts"></view>
 		</view>
+
+		<!-- <view class="content">
+		        
+		        <view class="echarts" :prop="option" :change:prop="echarts.updateEcharts" 
+		            id="echart">
+		        </view>
+		       
+		    </view> -->
+
 		<!-- 内容1 -->
-		<view class="album__content" style="background-color: white; display:flex;margin: 15px;">
+		<view class="album__content" style="background-color: white; display: flex; margin: 15px">
 			<!-- 左 -->
-			<view style="margin-top: 15px; margin-left: 15px;">
+			<view style="margin-top: 15px; margin-left: 15px">
 				<!-- 左上 -->
-				<view style="margin-right: 15px;width:220px;height:20px;">
+				<view style="margin-right: 15px; width: 220px; height: 20px">
 					<u--text text="江苏(姜堰)现代农业 (稻麦) 科技综合示范基地" bold size="11"></u--text>
 				</view>
 				<!-- 左下 -->
-				<view style="margin-top: 3vh; display:flex; ">
+				<view style="margin-top: 3vh; display: flex">
 					<view>
 						<image src="../../static/images/station/onlinebase/location.png"
-							style="width: 13px;height: 15px;margin-top:10px"></image>
+							style="width: 13px; height: 15px; margin-top: 10px"></image>
 					</view>
-					<view style="margin-left: 2vh;width:150px; height:20px; margin-top:10px">
+					<view style="
+              margin-left: 2vh;
+              width: 150px;
+              height: 20px;
+              margin-top: 10px;
+            ">
 						<u--text text="泰州市农业开发区江苏红旗种业" size="10"></u--text>
 					</view>
-
 				</view>
 			</view>
 			<!-- 右 -->
-			<view style=" margin-right: 15px;">
+			<view style="margin-right: 15px">
 				<!-- 右上 -->
-				<view style="margin-top: 15px;">
-					<button
-						style="font-size: 11px;background-color: #00ae67 ;color: white;width:80px;height: 30px;">查看详情</button>
+				<view style="margin-top: 15px">
+					<button style="
+              font-size: 11px;
+              background-color: #00ae67;
+              color: white;
+              width: 80px;
+              height: 30px;
+            ">
+						查看详情
+					</button>
 				</view>
 				<!-- 右下 -->
 				<view>
-					<button type="primary"
-						style="font-size: 11px; margin-bottom: 15px;margin-top: 10px;width:80px;height: 30px;">去这里</button>
+					<button type="primary" style="
+              font-size: 11px;
+              margin-bottom: 15px;
+              margin-top: 10px;
+              width: 80px;
+              height: 30px;
+            ">
+						去这里
+					</button>
 				</view>
 			</view>
-
-
 		</view>
 		<!-- 内容2 -->
-		<view class="album__content" style="background-color: white; display:flex;margin: 15px;">
+		<view class="album__content" style="background-color: white; display: flex; margin: 15px">
 			<!-- 左 -->
-			<view style="margin-top: 15px; margin-left: 15px;">
+			<view style="margin-top: 15px; margin-left: 15px">
 				<!-- 左上 -->
-				<view style="margin-right: 15px; width:220px; height:20px;">
+				<view style="margin-right: 15px; width: 220px; height: 20px">
 					<u--text text="江苏(姜堰)现代农业 (稻麦) 科技综合示范基地" bold size="11"></u--text>
 				</view>
 				<!-- 左下 -->
-				<view style="margin-top: 3vh; display:flex; ">
+				<view style="margin-top: 3vh; display: flex">
 					<view>
 						<image src="../../static/images/station/onlinebase/location.png"
-							style="width: 13px;height: 15px;margin-top:10px"></image>
+							style="width: 13px; height: 15px; margin-top: 10px"></image>
 					</view>
-					<view style="margin-left: 2vh; margin-top:10px;width:150px;height:20px;">
+					<view style="
+              margin-left: 2vh;
+              margin-top: 10px;
+              width: 150px;
+              height: 20px;
+            ">
 						<u--text text="泰州市农业开发区江苏红旗种业" size="10"></u--text>
 					</view>
-
 				</view>
 			</view>
 			<!-- 右 -->
-			<view style=" margin-right: 15px;">
+			<view style="margin-right: 15px">
 				<!-- 右上 -->
-				<view style="margin-top: 15px;">
-					<button
-						style="font-size: 11px;background-color: #00ae67 ;color: white;width:80px;height: 30px;">查看详情</button>
+				<view style="margin-top: 15px">
+					<button style="
+              font-size: 11px;
+              background-color: #00ae67;
+              color: white;
+              width: 80px;
+              height: 30px;
+            ">
+						查看详情
+					</button>
 				</view>
 				<!-- 右下 -->
 				<view>
-					<button type="primary"
-						style="font-size: 11px; margin-bottom: 15px;margin-top: 10px;width:80px;height: 30px;">去这里</button>
+					<button type="primary" style="
+              font-size: 11px;
+              margin-bottom: 15px;
+              margin-top: 10px;
+              width: 80px;
+              height: 30px;
+            ">
+						去这里
+					</button>
 				</view>
 			</view>
 		</view>
 	</view>
-
 </template>
 
-<script module="echarts" lang="renderjs">
-	const jsonData = require('../../static/map/ShanDong.json');
-	var myChart
+<script>
 	export default {
-		mounted() {
-			if (typeof window.echarts === 'function') {
-				this.mYChartmap()
-			} else {
-				// 动态引入较大类库避免影响页面展示
-				const script = document.createElement('script')
-				script.src = '../../static/echarts/echarts.min.js'
-				script.onload = this.mYChartmap.bind(this)
-				document.head.appendChild(script)
-
-			}
-		},
-		methods: {
-			mYChartmap() {
-				myChart = echarts.init(document.getElementById('echartsmap'))
-				echarts.registerMap('shandong', jsonData)
-				var option = {
+		// name: "LineEcharts",
+		data() {
+			return {
+				// 配置数据
+				option: {
 					grid: {
 						show: false,
 						left: '5%',
@@ -114,94 +142,37 @@
 						show: true,
 						roam: false, //是否允许鼠标滚动放大，缩小
 						map: 'shandong',
+						data: [],
 						label: {
 							//图形上的文本标签，可用于说明图形的一些数据信息
 							show: true, //是否显示文本
-							color: '#00ffff', //文本颜色
-							fontSize: 10,
+							color: '#CCC', //文本颜色
+							// fontSize: 20,
 						},
 						itemStyle: {
 							//地图区域的多边形 图形样式。 默认样试。
 							areaColor: '#00aa00', //地图区域的颜色。
-							borderColor: '#4F87F8', //边框线
-
-						},
-						emphasis: {
-							//高亮状态下的多边形和标签样式。
-							label: {
-								//文本
-								color: '#aa6c02',
-								fontSize: 14,
-							},
-
-							itemStyle: {
-								//区域
-								areaColor: '#aa4400',
+							borderColor: '#ffff00', //边框线
+							emphasis: {
+								areaColor: '#fff',
 							},
 						},
+						// emphasis: {
+						// 	//高亮状态下的多边形和标签样式。
+						// 	label: {
+						// 		//文本
+						// 		color: '#ADA',
+						// 	},
+						// 	itemStyle: {
+						// 		//区域
+						// 		areaColor: '#F60',
+						// 	},
+						// },
 					},
-					//是视觉映射组件，用于进行『视觉编码』，也就是将数据映射到视觉元素（视觉通道）。
-					// visualMap: {
-					// 	type: 'piecewise',
-					// 	pieces: [{
-					// 			gt: 201,
-					// 			label: '> 201',
-					// 			color: '#DE5D5F',
-					// 		},
-					// 		{
-					// 			gte: 101,
-					// 			lte: 200,
-					// 			label: '101 - 200',
-					// 			color: '#DE7152',
-					// 		},
-					// 		{
-					// 			gte: 10,
-					// 			lte: 100,
-					// 			label: '10 - 100',
-					// 			color: '#E8B63C',
-					// 		},
-					// 		{
-					// 			gte: 1,
-					// 			lt: 10,
-					// 			label: '1 - 9',
-					// 			color: '#50A3BA',
-					// 		},
-					// 	],
-					// 	min: 0, //最小值
-					// 	max: 1000, //最大值
-					// 	calculable: false, //是否显示拖拽用的手柄（手柄能拖拽调整选中范围）。
-					// 	splitNumber: 4,
-					// 	itemSymbol: 'circle',
-					// 	piecewise: {
-					// 		show: true,
-					// 	},
-					// 	inRange: {
-					// 		color: ['#50A3BA', '#E8B63C', '#DE5D5F'], //颜色
-					// 	},
-					// 	show: true,
-					// 	textStyle: {
-					// 		color: '#fff',
-					// 		fontSize: 8,
-					// 	},
-					// 	itemWidth: 8,
-					// 	inverse: false,
-					// 	precision: 0,
-					// 	itemHeight: 8,
-					// 	align: 'left',
-					// 	textGap: 5,
-					// 	showLabel: true,
-					// 	itemSymbol: 'circle',
-					// 	itemGap: 10,
-					// },
-					// legend: {
-					// 	orient: 'vertical',
-					// 	top: 'bottom',
-					// 	left: 'right',
-					// 	data: ['pm2.5'],
-					// 	textStyle: {
-					// 		color: '#fff',
-					// 	},
-					// },
+					tooltip: {
+						show: true,
+						formatter: `{b0}: {c0}`
+					},
 					series: [{
 						type: 'scatter', //样试
 						coordinateSystem: 'geo', //该系列使用的坐标系
@@ -244,6 +215,11 @@
 								show: false
 							}
 						},
+						onclick: function(params) {
+							// 处理点击事件的逻辑
+							console.log('1111');
+						},
+						// },
 						// geoIndex :[120.37,36.0944],
 						// symbol : 'circle',
 						// symbolSize : 10,
@@ -258,22 +234,76 @@
 						},
 						// hoverAnimation: true, //鼠标移入放大圆
 					}, ],
+
+
 				}
-				myChart.setOption(option)
 
+			};
+		},
 
-				myChart.on('click', function(params) {
-					console.log("111")
+		methods: {
+			// 动态改变配置数据
+			changeOption() {},
+		},
+	};
+</script>
+
+<script module="echarts" lang="renderjs">
+	// 将npm方式下载的echarts插件引入进来
+	import * as echarts from 'echarts';
+	// Vue.prototype.$echarts = echarts;
+	const jsonData = require('../../static/map/ShanDong.json');
+	
+	export default {
+
+		mounted() {
+			let that = this;
+			// 通过nextTick异步画图
+			this.$nextTick(() => {
+				that.mYChartmap()
+			});
+
+			if (typeof window.echarts === 'function') {
+				this.mYChartmap()
+			} else {
+				// 动态引入较大类库避免影响页面展示
+				const script = document.createElement('script')
+				script.src = '../../static/echarts/echarts.min.js'
+				script.onload = this.mYChartmap.bind(this)
+				document.head.appendChild(script)
+
+			}
+		},
+
+		methods: {
+			// 初始化
+			mYChartmap() {
+				this.myChart = echarts.init(document.getElementById('echartsmap'))
+				this.myChart.setOption(this.option);
+				echarts.registerMap('shandong', jsonData)
+
+				this.myChart.on('click', function(params) {
+					// console.log("111")
 				})
-				myChart.on('click', function(params) {
+				this.myChart.on('click', function(params) {
 					const idx = params.dataIndex;
 					console.log(idx, params.name, params.value);
 					// to do
 				})
+
+
+
+				// 这里是用于窗口变化时的自适应，利用的是echarts自带的resize方法
+				window.addEventListener('resize', () => {
+					this.myChart.resize()
+				});
 			},
+
+			// 监听配置数据变化，并重新渲染
 			updateEcharts(newValue, oldValue, ownerInstance, instance) {
-				// 监听 service 层数据变更
-				// console.log("111")
+				if (this.myChart) {
+					this.myChart.setOption(newValue);
+				}
 				echarts.registerMap('shandong', jsonData)
 				myChart.setOption(newValue)
 			},
@@ -283,7 +313,10 @@
 					test: 'test'
 				})
 			},
-		}
+		},
+
+
+
 	}
 </script>
 
@@ -310,8 +343,8 @@
 		height: 100%;
 	}
 
-	#echartsmap {
-		width: 682rpx;
-		height: 600rpx;
+	.echarts {
+		width: 100%;
+		height: 500rpx;
 	}
 </style>
