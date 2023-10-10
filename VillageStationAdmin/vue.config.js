b1050bbd90fcd647ea1a7e118dfb6ea4c5ab662b
sunshouxin <1585410279@qq.com>
2023-10-10 10:16:14 +0800

    Add

--- VillageStationAdmin/vue.config.js
+++ VillageStationAdmin/vue.config.js
@@ -35,6 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
+        // target: `http://xcyz.nat300.top`,
         target: `http://localhost:8080`,
         changeOrigin: true,
         pathRewrite: {
