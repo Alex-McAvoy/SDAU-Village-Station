bdf6253a25361a32815fdd57b5fe83a19ca1ee50
Alex-McAvoy <931772462@qq.com>
2023-10-22 14:32:44 +0800

    Delete VillageStationAdmin directory

--- VillageStationAdmin/src/utils/fixedSize.js
+++
@@ -1,8 +0,0 @@
-// 控制显示字的长度
-export function fixedSize(content) {
-    if(content!=null){
-        if(content.length<45) return content;
-        else return content.substring(0, 45) + "..."
-    }
-    return content;
-}
