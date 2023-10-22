0feda151966a3ea9be842d2362f665977c4a2d7f
Alex-McAvoy <931772462@qq.com>
2023-10-23 00:43:43 +0800

    Add files via upload

---
+++ VillageStationApp/config.js
@@ -0,0 +1,12 @@
+// 应用全局配置
+module.exports = {
+	transpileDependencies: ['uview-ui'],
+    baseUrl: 'http://202.194.131.202:5500',
+    // baseUrl: 'http://localhost:8080',
+	appInfo: {
+		// 应用名称
+		name: "village-station-app",
+		// 应用版本
+		version: "1.0.0",
+	},
+}
