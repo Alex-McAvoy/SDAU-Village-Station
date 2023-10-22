c7c8ef30da07bcdb270b39b9dccd5f632dccee3b
Alex-McAvoy <931772462@qq.com>
2023-10-23 00:45:21 +0800

    Update config.js

--- VillageStationApp/config.js
+++ VillageStationApp/config.js
@@ -1,8 +1,7 @@
 // 应用全局配置
 module.exports = {
 	transpileDependencies: ['uview-ui'],
-    baseUrl: 'http://202.194.131.202:5500',
-    // baseUrl: 'http://localhost:8080',
+	baseUrl: 'http://localhost:8080',
 	appInfo: {
 		// 应用名称
 		name: "village-station-app",
